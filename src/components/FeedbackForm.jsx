4ea37a1bf3fa19f49a958d8f7cffe3234af9f5e0
Paulo Ilarde <pauloilarde@gmail.com>
2022-01-07 16:41:06 -0300

    Criando componente de texto das reviews

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Card from "./shared/Card";
+import Button from "./shared/Button";
+
+function FeedbackForm() {
+  const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+
+  const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length < 10) {
+      setBtnDisabled(true);
+      setMessage("Text must be at least 10 characters");
+    } else {
+      setBtnDisabled(false);
+      setMessage(null);
+    }
+
+    setText(e.target.value);
+  };
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your experience with our service?</h2>
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write your review"
+            value={text}
+          />
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className="message"> {message} </div>}
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
