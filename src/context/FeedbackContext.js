fb1f01faed8ff45faa52d6316b62247dc9c7fb8d
Paulo Ilarde <pauloilarde@gmail.com>
2022-01-11 19:09:07 -0300

    Centralizando as funções no context

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
@@ -6,14 +7,38 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: "This text here is from context",
+      text: "This is item context 1",
       rating: 10,
     },
+    {
+      id: 2,
+      text: "This is item context 2",
+      rating: 9,
+    },
+    {
+      id: 3,
+      text: "This is item context 3",
+      rating: 7,
+    },
   ]);
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        addFeedback,
+        deleteFeedback,
       }}
     >
       {children}
