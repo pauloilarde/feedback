20a2c2b96b0e4b66f895639d28d8292a6df7c556
Paulo Ilarde <pauloilarde@gmail.com>
2022-01-11 21:26:24 -0300

    Finalizando o uso de context nesse projeto

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -22,6 +22,11 @@ export const FeedbackProvider = ({ children }) => {
     },
   ]);
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
@@ -33,12 +38,28 @@ export const FeedbackProvider = ({ children }) => {
     }
   };
 
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
+  };
+
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         addFeedback,
         deleteFeedback,
+        editFeedback,
+        updateFeedback,
       }}
     >
       {children}
